6663e2bac4490ab2394b421f6e7a3f574971b06c
specnats <specnats@hotmail.fi>
2015-02-12 23:13:34 +0200

    Animation advance

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,20 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	private Animator animator; 
+	//private Animator animator; 
 	
 	// Use this for initialization 
 	
-	void Start () { animator = this.GetComponent(); } 
+	//void Start () { animator = this.GetComponent(); } 
 	
 	// Update is called once per frame 
 	
-	void Update () { float v = Input.GetAxis("Vertical"); 
-		float h = Input.GetAxis("Horizontal"); ManageMovement(h, v); } void ManageMovement(float horizontal,float vertical) { if (horizontal != 0f || vertical != 0f) { animator.SetBool ("moving", true); animateWalk (horizontal, vertical); } else { animator.SetBool ("moving", false); } Vector3 movement = new Vector3 (horizontal,vertical, 0); rigidbody2D.velocity = movement; } void animateWalk(float h,float v) { if(animator){ if ((v > 0)&&(v>h)) { animator.SetInteger ("Direction", 1); } if ((h > 0)&&(v<h)) { animator.SetInteger ("Direction", 2); } if ((v < 0)&&(v<h)) { animator.SetInteger ("Direction", 3); } if ((h < 0 )&&(v>h)) { animator.SetInteger ("Direction", 4); } } } }
+	void Update () { 
+		float v = Input.GetAxis("Vertical"); 
+		float h = Input.GetAxis("Horizontal"); ManageMovement(h, v); 
+	} 
+
+	void ManageMovement (float horizontal, float vertical) {
+		if (horizontal != 0f || vertical != 0f)
+			}}
+	//	{ animator.SetBool ("moving", true); animateWalk (horizontal, vertical); } else { animator.SetBool ("moving", false); } Vector3 movement = new Vector3 (horizontal,vertical, 0); rigidbody2D.velocity = movement; } void animateWalk(float h,float v) { if(animator){ if ((v > 0)&&(v>h)) { animator.SetInteger ("Direction", 1); } if ((h > 0)&&(v<h)) { animator.SetInteger ("Direction", 2); } if ((v < 0)&&(v<h)) { animator.SetInteger ("Direction", 3); } if ((h < 0 )&&(v>h)) { animator.SetInteger ("Direction", 4); } } } }
