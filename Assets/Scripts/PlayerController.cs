1136979ec69660a0a341b4668c505ac001c49768
specnats <specnats@hotmail.fi>
2015-02-27 00:44:48 +0200

    Animation and movement working
    Bloody finally

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,14 @@
 ﻿using UnityEngine; 
-using System.Collections; 
-public class playerController : MonoBehaviour { private Animator animator; // Use this for initialization 
-	void Start(){ animator = (Animator)this.GetComponent (typeof(Animator)); 
-	} // Update is called once per frame 
-	void Update () { float v = Input.GetAxis("Vertical"); float h = Input.GetAxis("Horizontal"); ManageMovement(h, v); 
-	} 
-	void ManageMovement(float horizontal,float vertical) 
-	{ 
-		if (horizontal != 0f || vertical != 0f) { animator.SetBool ("moving", true); animateWalk (horizontal, vertical); 
-		} 
-		else { animator.SetBool ("moving", false); } Vector3 movement = new Vector3 (horizontal,vertical, 0); rigidbody2D.velocity = movement;
-	}
-	void animateWalk(float h,float v)
-	{ 
-		if(animator)
-		{ 
-			if ((v > 0)&&(v>h)) { animator.SetInteger ("direction", 1); 
-			} 
-			if ((h > 0)&&(v<h)) { animator.SetInteger ("direction", 2); 
-			} 
-			if ((v < 0)&&(v<h)) { animator.SetInteger ("direction", 3); 
-			} 
-			if ((h < 0)&&(v>h)) { animator.SetInteger ("direction", 4); 
-			} 
-		} 
-	} 
-} 
+using System.Collections; public class playerController : MonoBehaviour { private Animator animator; // Use this for initialization 
+	void Start () { animator = (Animator)this.GetComponent (typeof(Animator)); } // Update is called once per frame 
+	void Update () { float v = Input.GetAxis("Vertical"); float h = Input.GetAxis("Horizontal"); ManageMovement(h, v); } 
+	void ManageMovement(float horizontal,float vertical) { 
+		if (horizontal != 0f || vertical != 0f) { animator.SetBool ("moving", true); animateWalk (horizontal, vertical); } 
+		else { animator.SetBool ("moving", false); } 
+		Vector3 movement = new Vector3 (horizontal,vertical, 0); rigidbody2D.velocity = movement; } 
+	void animateWalk(float h,float v) { 
+		if(animator){ 
+			if ((v > 0)&&(v>h)) { animator.SetInteger ("direction", 1); } 
+			if ((h > 0)&&(v<h)) { animator.SetInteger ("direction", 2); } 
+			if ((v < 0)&&(v<h)) { animator.SetInteger ("direction", 3); } 
+			if ((h < 0)&&(v>h)) { animator.SetInteger ("direction", 4); } } } }
